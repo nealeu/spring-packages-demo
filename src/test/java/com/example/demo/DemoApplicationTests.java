dfd026499f275d7fc00bb692b72d2860e73ceb67
Neale Upstone <neale@nealeupstone.com>
2019-01-26 13:14:37 +0000

    Flesh out tests including Basic Auth security
    For internal micro-services we could use add spring-security-oauth2-boot and configure jwt.key-value instead.
    
    See https://docs.spring.io/spring-security-oauth2-boot/docs/current/reference/htmlsingle/#boot-features-security-oauth2-resource-server

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,55 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    private WebTestClient autenticatingWebClient;
+
+    @Before
+    public void init() {
+        autenticatingWebClient = webClient.mutate()
+                .filter(basicAuthentication("test-user", "tust-pazvord"))
+                .build();
+    }
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void cannotAccessWithoutAuthentication() {
+        webClient.get().uri("/packages").accept(MediaType.APPLICATION_JSON_UTF8).exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    public void canCheckHealthWithoutAuthentication() {
+        webClient.get().uri("/actuator/health").accept(MediaType.APPLICATION_JSON_UTF8).exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void canGetListOfPackages() {
+        autenticatingWebClient.get().uri("/packages").accept(MediaType.APPLICATION_JSON_UTF8).exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .json("{\"id\":1,\"name\":\"package name\",\"description\":\"package description\"}");
+    }
 }
 
