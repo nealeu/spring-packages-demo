4cf39f3de7e23d22cdd0c4a2d3f99af5c8f25e3c
Neale Upstone <neale@nealeupstone.com>
2019-01-27 22:12:14 +0000

    Configure Http Security to be stateless so we don't create a session

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +15,15 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                super.configure(http);
+                http.sessionManagement().sessionCreationPolicy(STATELESS);
+            }
+        };
+    }
 }
 
